e457f1b723fad05e4e3c3881a07741e14f73079a
Kelong Cong <kelongcong@gmail.com>
2015-03-09 18:08:23 +0000

    removed AXOLOT_NULL_POINTER, user should be responsible to for passing in non-null params

--- src/curve.c
+++ src/curve.c
@@ -10,9 +10,6 @@ static const int CURVE_DJB_TYPE = 0x05;
 
 int curve_generate_keypair(struct curve_key_pair* pair)
 {
-	if (pair == NULL)
-		return AXOLOTL_NULL_POINTER;
-
 	randombytes_buf(pair->sk.bytes, CURVE_KEY_BYTES_LEN);
 	pair->sk.bytes[0] &= 248;
 	pair->sk.bytes[31] &= 127;
@@ -30,7 +27,6 @@ int curve_generate_keypair(struct curve_key_pair* pair)
 int curve_decode_point(struct curve_pk* pk,
 		const unsigned char* bytes, const int offset)
 {
-	// TODO NULL check
 	int type = bytes[offset] & 0xff;
 
 	if (type == CURVE_DJB_TYPE) {
@@ -45,9 +41,6 @@ int curve_decode_point(struct curve_pk* pk,
 int curve_decode_private_point(struct curve_sk* sk,
 		const unsigned char* bytes, const size_t byteslen)
 {
-	if (bytes == NULL || sk == NULL)
-		return AXOLOTL_NULL_POINTER;
-
 	if (byteslen > CURVE_KEY_BYTES_LEN)
 		return AXOLOTL_INVALID_KEYLEN;
 
@@ -95,9 +88,6 @@ int curve_calculate_signature(unsigned char* sig, const struct curve_sk* sk,
 // out must have at least length of 33, or CURVE_KEY_BYTES_LEN + 1
 int curve_serialize_pk(unsigned char* out, const struct curve_pk* pk)
 {
-	if (pk == NULL || out == NULL)
-		return AXOLOTL_NULL_POINTER;
-
 	out[0] = (unsigned char) pk->type;
 	memcpy(out+1, pk->bytes, CURVE_KEY_BYTES_LEN);
 	return 0;
@@ -106,9 +96,6 @@ int curve_serialize_pk(unsigned char* out, const struct curve_pk* pk)
 // out must have at least CURVE_KEY_BYTES_LEN
 int curve_serialize_sk(unsigned char* out, const struct curve_sk* sk)
 {
-	if (sk == NULL || out == NULL)
-		return AXOLOTL_NULL_POINTER;
-
 	memcpy(out, sk->bytes, CURVE_KEY_BYTES_LEN);
 	return 0;
 }
